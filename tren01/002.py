c30bd7bc77400da9175a284b7f0ae2018529c247
Андрей Беляков <ttxiom@gmail.com>
2020-10-06 14:18:40 +0500

    new

--- tren01/002.py
+++ tren01/002.py
@@ -1,17 +1,16 @@
-x, y, z, w = map(int, input().split())
-
-lst = set()
-
 def get(a, b, c):
     global lst
     if a * x + b * y + c * z == w:
         lst.add( (a,b,c) )
-    if (a+1)*x <= w:
+    if (a+1)*x + b*y + c*z <= w:
         get(a + 1, b, c)
-    if (b+1)*y <= w:
+    if a*x + (b+1)*y + c*z <= w:
         get(a, b + 1, c)
-    if (c+1)*z <= w:
+    if a*x + b*y + (c+1)*z <= w:
         get(a, b, c + 1)
 
+
+x, y, z, w = map(int, input().split())
+lst = set()
 get(0, 0, 0)
 print(len(lst))
